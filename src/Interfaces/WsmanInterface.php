52e117cd42bff4958fbe79031cf1ef15f9eaaebe
c0py <danny.connolly@gmail.com>
2017-11-02 23:22:26 +0000

    Update WsmanInterface.php

--- src/Interfaces/WsmanInterface.php
+++ src/Interfaces/WsmanInterface.php
@@ -5,18 +5,18 @@ namespace c0py\Wsman\Interfaces;
 interface WsmanInterface
 {
     public function __construct($host, $username, $password);
-    
-	public function connect($namespace);
-    
-	public function getSession();
-    
-	public function getHost();
-    
-	public function getUsername();
-    
-	public function setHost($host);
-    
-	public function setUsername($username);
-    
-	public function setPassword($password);
+
+    public function connect();
+
+    public function getSession();
+
+    public function getHost();
+
+    public function getUsername();
+
+    public function setHost($host);
+
+    public function setUsername($username);
+
+    public function setPassword($password);
 }
